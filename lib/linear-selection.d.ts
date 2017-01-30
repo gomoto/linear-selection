a2514b74ef441630fb2ca4d597a434f848b252e5
Ryan Gomoto <ryan@cyrusbio.com>
2017-01-30 15:48:51 -0800

    0.0.2

--- lib/linear-selection.d.ts
+++ lib/linear-selection.d.ts
@@ -1,19 +1,22 @@
 export default class LinearSelection {
-    private _selection;
+    private _selections;
     private _anchor;
     private _touchMode;
     private _pendingPositions;
-    constructor(size?: number);
-    setSize(size: number): void;
-    insert(count: number, start: number, selected: boolean): void;
+    constructor();
+    reset(): void;
     touch(index: number, options?: TouchOptions): void;
+    readonly min: number;
+    readonly max: number;
+    readonly size: number;
     isSelected(index: number): boolean;
-    private unselectAll(exception?);
+    private _select(index);
+    private _unselect(index);
+    private _unselectAll(exception?);
     private _click(index, exclusive);
     private _shiftClick(index, exclusive);
     private _acceptPending();
     private _rejectPending();
-    private static fillArray(array, value);
 }
 export interface TouchOptions {
     shift?: boolean;
